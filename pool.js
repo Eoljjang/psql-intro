f55cfac0fdfffc31ca24b49c66252e1caad88dc2
Nathan <nathan.wong2002@gmail.com>
2025-05-02 15:04:00 -0600

    changed to upper case P for pool

--- pool.js
+++ pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
-// A "pool" is a collection of clients - it holds onto those connections.
+// A "pool" is a collectaion of clients - it holds onto those connections.
 module.exports = new Pool({
   host: "localhost", // or wherever the db is hosted
   user: "nathan_pulsemedica",
